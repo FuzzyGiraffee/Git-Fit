cbf4ad513e4b81b6ebb52a7096d18bf982ff1d29
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-05-03 19:03:42 -0500

    Skeleton code

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const templateRoutes = require('./homeRoutes');
+
+router.use('/', templateRouts);
+router.use('/api', apiRoutes);
+
+module.exports = router;
