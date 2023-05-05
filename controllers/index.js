09c698dd5fe9daa684c02858387f931350d51a44
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-05-04 20:45:14 -0500

    ncie

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 
 const templateRoutes = require('./homeRoutes');
 
-router.use('/', templateRouts);
+router.use('/', templateRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
