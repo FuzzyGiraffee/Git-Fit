a483d9299f1a40d3bda63ab35ebe9f8517ee6226
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-05-10 03:26:13 -0500

    added calendar

---
+++ public/js/calendar.js
@@ -0,0 +1,20 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+
+
+let calendar = new Calendar(calendarEl, {
+  plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,listWeek'
+  }
+ 
+  calen
+});
+
+
